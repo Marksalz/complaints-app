0a07bf0317b446dee76bef9b24a0015154a225c9
Mark <menachemsalzberg13@gmail.com>
2025-08-07 16:52:25 +0300

    Refactor complaint submission logic and update admin form method

--- controllers/complaintsController.js
+++ controllers/complaintsController.js
@@ -1,9 +1,43 @@
+import client from "../db/connect.js";
+
+const complaintsCollectipn = client.collection("complaints");
+
 export function submitContoller(req, res) {
-  const complaint = req.body;
-  res.send(`Complaint: ${complaint} received! Thank you.`);
+  const { complaint_category, complaint_text } = req.body;
+  const complaint = {
+    complaint_category,
+    complaint_text,
+    createdAt: new Date(),
+  };
+  complaintsCollectipn
+    .insertOne(complaint)
+    .then(() => {
+      res.send("Complaint received! Thank you.");
+    })
+    .catch((err) => {
+      res.status(500).send("Error saving complaint");
+    });
 }
 
-export function adminController(req, res) {
-  // Handle admin form submission
-  res.send("Admin request received.");
+export async function adminController(req, res) {
+  const password = req.body.adminPassword;
+  if (password === process.env.ADMIN_PASSWORD) {
+    complaintsCollectipn
+      .find({})
+      .toArray()
+      .then((complaints) => {
+        res.send(
+          `<pre style='color:green; justify-content:center; margin-top:5vh; font-size:20px'>${JSON.stringify(
+            complaints,
+            null,
+            2
+          )}</pre>`
+        );
+      })
+      .catch((err) => {
+        res.status(500).send("Error retrieving complaints");
+      });
+  } else {
+    res.status(404).sendFile("/public/not-found.html", { root: process.cwd() });
+  }
 }
